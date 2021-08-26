90ec66fefc1ba5564dab03524c34c9fe6ca5b8e7
mnilg <mnilga@gmail.com>
2021-08-26 17:38:36 +0300

    implemented method of OrderService

--- src/main/java/com/mnilga/travel/agency/application/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mnilga/travel/agency/application/service/impl/OrderServiceImpl.java
@@ -1,20 +1,24 @@
 package com.mnilga.travel.agency.application.service.impl;
 
 import com.mnilga.travel.agency.application.dto.OrderDto;
-import com.mnilga.travel.agency.application.dto.UserDto;
+import com.mnilga.travel.agency.application.exceptions.ResourceNotFoundException;
 import com.mnilga.travel.agency.application.model.*;
+import com.mnilga.travel.agency.application.repository.OrderRepository;
 import com.mnilga.travel.agency.application.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    private OrderRepository orderRepository;
     private ConversionService service;
 
     @Autowired
@@ -22,42 +26,70 @@ public class OrderServiceImpl implements OrderService {
         this.service = service;
     }
 
-    public void testDto(){
-        Order order = new Order();
-        order.setId(UUID.randomUUID());
-        order.setArrivalDate(LocalDate.now());
-        order.setDepartureDate(LocalDate.now());
-        order.setOrderDate(LocalDate.now());
-        order.setUser(new User());
-        order.setHotel(new Hotel());
-        order.setRoom(new Room());
-
-        OrderDto orderDto = service.convert(order, OrderDto.class);
-        System.out.println(orderDto);
+    @Autowired
+    public void setOrderRepository(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
     }
 
     @Override
-    public void addOrder(Order order) {
-
+    public OrderDto create(Order order) {
+        Order newOrder = orderRepository.save(order);
+        return service.convert(newOrder, OrderDto.class);
     }
 
     @Override
-    public OrderDto getOrderById(String id) {
+    public OrderDto readById(UUID id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Order with id = " + id + " not found!"));
+        return service.convert(order, OrderDto.class);
+    }
+
+    @Override
+    public OrderDto update(Order order) {
+        if (order == null) {
+            throw new RuntimeException("Order can't be null");
+        }
+
+        Optional<Order> optionalOrder = orderRepository.findById(order.getId());
+        if (optionalOrder.isEmpty()) {
+            throw new ResourceNotFoundException("Order with id = " + order.getId() + " does not exist!");
+        }
+
+        Order updatedOrder = orderRepository.save(order);
+        return service.convert(updatedOrder, OrderDto.class);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Order with id = " + id + " not found!"));
+        orderRepository.delete(order);
+    }
+
+    @Override
+    public List<OrderDto> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream()
+                .map(order -> service.convert(order, OrderDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public OrderDto patch(Map<String, Object> fields, UUID id) {
         return null;
     }
 
-    @Override
-    public List<OrderDto> getOrdersByUserId(UUID userId) {
-        return null;
-    }
 
-    @Override
-    public List<OrderDto> getOrdersByHotelId(UUID hotelId) {
-        return null;
-    }
-
-    @Override
-    public void deleteOrderById(UUID id) {
-
-    }
+//    public void testDto(){
+//        Order order = new Order();
+//        order.setId(UUID.randomUUID());
+//        order.setArrivalDate(LocalDate.now());
+//        order.setDepartureDate(LocalDate.now());
+//        order.setOrderDate(LocalDate.now());
+//        order.setUser(new User());
+//        order.setHotel(new Hotel());
+//        order.setRoom(new Room());
+//        OrderDto orderDto = service.convert(order, OrderDto.class);
+//        System.out.println(orderDto);
+//    }
 }
