a4fb692b3a5948fbe2fa31e7bc03d734ed31e072
mnilg <mnilga@gmail.com>
2021-08-26 18:35:21 +0300

    added logging aop

---
+++ src/main/java/com/mnilga/travel/agency/application/config/MyAspect.java
@@ -0,0 +1,31 @@
+package com.mnilga.travel.agency.application.config;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+
+@Aspect
+@Component
+public class MyAspect {     //for logging
+
+    @Pointcut("within(com.mnilga.travel.agency.application..*)")
+    public void callAtMyServicePublic() {
+    }
+
+    @Around("callAtMyServicePublic()")
+    public Object aroundCallAt(ProceedingJoinPoint call) throws Throwable {
+        StopWatch clock = new StopWatch(call.toString());
+        try {
+            clock.start(call.toShortString());
+            return call.proceed();
+        } finally {
+            clock.stop();
+            System.out.println(clock.prettyPrint());
+        }
+    }
+}
