207cc9b694b7b672fa584bbcf9e674ca9a17acb5
mnilg <mnilga@gmail.com>
2021-08-26 16:19:48 +0300

    implemented methods of UserService

--- src/main/java/com/mnilga/travel/agency/application/service/impl/UserServiceImpl.java
+++ src/main/java/com/mnilga/travel/agency/application/service/impl/UserServiceImpl.java
@@ -1,68 +1,108 @@
 package com.mnilga.travel.agency.application.service.impl;
 
-import com.mnilga.travel.agency.application.dto.RoleDto;
+
 import com.mnilga.travel.agency.application.dto.UserDto;
+import com.mnilga.travel.agency.application.exceptions.ResourceNotFoundException;
 import com.mnilga.travel.agency.application.model.Role;
 import com.mnilga.travel.agency.application.model.User;
+import com.mnilga.travel.agency.application.repository.UserRepository;
 import com.mnilga.travel.agency.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private UserRepository userRepository;
     private ConversionService service;
+    private RoleServiceImpl roleService;
 
     @Autowired
     public void setService(ConversionService service) {
         this.service = service;
     }
 
-    public void testDto(){
-        User user = new User();
-        user.setId(UUID.randomUUID());
-        user.setEmail("mnilga@gmail.com");
-        user.setFirstName("Maya");
-        user.setLastName("Nilga");
-        user.setSex(User.Sex.FEMALE);
-        user.setRole(new Role());
-        UserDto userDto = service.convert(user, UserDto.class);
-        System.out.println(userDto);
-    }
-
-
     @Override
     public UserDto create(User user) {
-        return null;
+        Role role = roleService.findByName("USER");
+        user.setRole(role);
+        User newUser = userRepository.save(user);
+        return service.convert(newUser, UserDto.class);
     }
 
     @Override
     public UserDto readById(UUID id) {
-        return null;
+        User user = findUserById(id);
+        return service.convert(user, UserDto.class);
+    }
+
+    private User findUserById(UUID id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new ResourceNotFoundException("User with id = " + id + " not found");
+        }
+        return optionalUser.get();
     }
 
     @Override
     public UserDto update(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
+
+        Optional.ofNullable(userRepository.findByLogin(user.getEmail()))
+                .orElseThrow(() -> new ResourceNotFoundException("User with email = " + user.getEmail() + " does not exist!"));
+
+        User updatedUser = userRepository.save(user);
+        return service.convert(updatedUser, UserDto.class);
     }
 
     @Override
     public void delete(UUID id) {
-
+        User user = findUserById(id);
+        userRepository.delete(user);
     }
 
     @Override
     public UserDto patch(Map<String, Object> fields, UUID id) {
-        return null;
+        User user = findUserById(id);
+        fields.forEach((k, v) -> {
+            Field field = ReflectionUtils.findField(User.class, k);
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, user, v);
+        });
+        User patchedUser = userRepository.save(user);
+        return service.convert(patchedUser, UserDto.class);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return null;
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user -> service.convert(user, UserDto.class))
+                .collect(Collectors.toList());
     }
+
+
+//    public void testDto(){
+//        User user = new User();
+//        user.setId(UUID.randomUUID());
+//        user.setEmail("mnilga@gmail.com");
+//        user.setFirstName("Maya");
+//        user.setLastName("Nilga");
+//        user.setSex(User.Sex.FEMALE);
+//        user.setRole(new Role());
+//        UserDto userDto = service.convert(user, UserDto.class);
+//        System.out.println(userDto);
+//    }
+
 }
