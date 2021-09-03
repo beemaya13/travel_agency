a1fd4535155aacf8c425dc875fb453671cd3be9c
mnilg <mnilga@gmail.com>
2021-09-03 18:31:34 +0300

    added logging messages

--- src/main/java/com/mnilga/travel/agency/application/service/impl/UserServiceImpl.java
+++ src/main/java/com/mnilga/travel/agency/application/service/impl/UserServiceImpl.java
@@ -7,7 +7,10 @@ import com.mnilga.travel.agency.application.model.Role;
 import com.mnilga.travel.agency.application.model.User;
 import com.mnilga.travel.agency.application.repository.UserRepository;
 import com.mnilga.travel.agency.application.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final Logger LOGGER = LogManager.getLogger(UserServiceImpl.class);
     private UserRepository userRepository;
     private ConversionService service;
     private RoleServiceImpl roleService;
@@ -44,8 +48,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto create(User user) {
+        LOGGER.info("Starting to create user");
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
         getRoleFromUser(user);
         User newUser = userRepository.save(user);
+        LOGGER.info("User is created");
         return service.convert(newUser, UserDto.class);
     }
 
@@ -57,15 +66,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findUserById(UUID id) {
+        LOGGER.info("Starting to find user with id " + id);
         Optional<User> optionalUser = userRepository.findById(id);
         if (optionalUser.isEmpty()) {
             throw new ResourceNotFoundException("User with id = " + id + " not found");
         }
+        LOGGER.info("User with id " + id + " found");
         return optionalUser.get();
     }
 
     @Override
     public UserDto update(User user) {
+        LOGGER.warn("Starting to update user with id + " + user.getId());
         if (user == null) {
             throw new RuntimeException("User can't be null");
         }
@@ -75,17 +87,22 @@ public class UserServiceImpl implements UserService {
 
         getRoleFromUser(user);
         User updatedUser = userRepository.save(user);
+        LOGGER.info("User with id + " + user.getId() + " is updated");
         return service.convert(updatedUser, UserDto.class);
     }
 
     @Override
+    @CacheEvict   // to delete deleted users from cache
     public void delete(UUID id) {
+        LOGGER.warn("Starting to delete user with id " + id);
         User user = findUserById(id);
         userRepository.delete(user);
+        LOGGER.info("User with id " + id + " is deleted") ;
     }
 
     @Override
     public UserDto patch(Map<String, Object> fields, UUID id) {
+        LOGGER.warn("Started patching user with id + " + id);
         User user = findUserById(id);
         fields.forEach((k, v) -> {
             Field field = ReflectionUtils.findField(User.class, k);
@@ -93,29 +110,36 @@ public class UserServiceImpl implements UserService {
             ReflectionUtils.setField(field, user, v);
         });
         User patchedUser = userRepository.save(user);
+        LOGGER.info("User with id + " + user.getId() + " is patched");
         return service.convert(patchedUser, UserDto.class);
     }
 
     @Override
     @Cacheable(value = "users-cache")
     public List<UserDto> getAllUsers() {
+        LOGGER.info("Starting to get all users");
         List<User> users = userRepository.findAll();
+        LOGGER.info("All users are got");
         return users.stream()
                 .map(user -> service.convert(user, UserDto.class))
                 .collect(Collectors.toList());
     }
 
     private void getRoleFromUser(User userWithRole) {
+        LOGGER.info("Starting to get role from user by name");
         String roleName = userWithRole.getRole().getName();
         Role role;
         try {
             role = roleService.findByName(roleName);
         } catch (ResourceNotFoundException exception) {
+            LOGGER.error("No matching roles found. Setting default user role");
             role = roleService.findByName("USER");
+            LOGGER.info("USER role is set");
             userWithRole.setRole(role);
             return;
         }
         userWithRole.setRole(role);
+        LOGGER.info("Role is set");
     }
 
 
