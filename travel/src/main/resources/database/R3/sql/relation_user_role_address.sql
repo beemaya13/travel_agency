09d65df4305241a3ccc1ca41741ddf249f10f2dd
mnilg <mnilga@gmail.com>
2021-09-11 18:10:42 +0300

    added R3/dml.xml

---
+++ travel/src/main/resources/database/R3/sql/relation_user_role_address.sql
@@ -0,0 +1,19 @@
+-- update users
+-- set address_id = null,
+--     role_id = null
+-- where true;
+--
+
+update users
+set role_id = random_values.role_id,
+    address_id = random_values.address_id
+FROM users u
+CROSS JOIN LATERAL (
+    SELECT roles.id as role_id,
+           addresses.id as address_id
+    FROM roles, addresses
+    where u.id is not null
+    order by RANDOM()
+    limit 1
+    ) random_values
+WHERE users.role_id is null or users.address_id is null;
