fd25e5fc6caf77bb914e603f6069b36e3b179139
mnilg <mnilga@gmail.com>
2021-09-23 16:54:53 +0300

    added dependency for apache beam

---
+++ apache_beam/src/main/java/com/mnilga/travel/agency/apachebeam/BeamdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mnilga.travel.agency.apachebeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamdemoApplication.class, args);
+	}
+
+}
