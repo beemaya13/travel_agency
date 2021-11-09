ba370521767f6d7aea1f76c01e2358cf9fc3acb1
mnilg <mnilga@gmail.com>
2021-11-10 00:18:59 +0200

    clean up

--- travel/src/main/java/com/mnilga/travel/agency/application/TravelAgencyApplication.java
+++ travel/src/main/java/com/mnilga/travel/agency/application/TravelAgencyApplication.java
@@ -1,59 +1,13 @@
 package com.mnilga.travel.agency.application;
 
-import com.mnilga.travel.agency.application.service.impl.*;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TravelAgencyApplication {
 
-	//for testing dto converters
-	private RoleServiceImpl roleServiceImpl;
-	private UserServiceImpl userServiceImpl;
-	private HotelServiceImpl hotelServiceImpl;
-	private RoomServiceImpl roomServiceImpl;
-	private OrderServiceImpl orderServiceImpl;
-
-
-	@Autowired
-	public void setRoleServiceImpl(RoleServiceImpl roleServiceImpl) {
-		this.roleServiceImpl = roleServiceImpl;
-	}
-
-	@Autowired
-	public void setUserServiceImpl(UserServiceImpl userServiceImpl) {
-		this.userServiceImpl = userServiceImpl;
-	}
-
-	@Autowired
-	public void setHotelServiceImpl(HotelServiceImpl hotelServiceImpl) {
-		this.hotelServiceImpl = hotelServiceImpl;
-	}
-
-	@Autowired
-	public void setRoomServiceImpl(RoomServiceImpl roomServiceImpl) {
-		this.roomServiceImpl = roomServiceImpl;
-	}
-
-	@Autowired
-	public void setOrderService(OrderServiceImpl orderServiceImpl) {
-		this.orderServiceImpl = orderServiceImpl;
-	}
-
-	//for testing dto converters    + in Audit class init field UUID
-	@EventListener(ApplicationReadyEvent.class)
-	public void test() {
-		//userServiceImpl.testDto();
-		//roleServiceImpl.testDto();
-		//hotelServiceImpl.testDto();
-		//roomServiceImpl.testDto();
-		//orderServiceImpl.testDto();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(TravelAgencyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyApplication.class, args);
+    }
 }
--- travel/src/main/java/com/mnilga/travel/agency/application/config/Config.java
+++ travel/src/main/java/com/mnilga/travel/agency/application/config/Config.java
@@ -8,7 +8,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+
 @ComponentScan
 @Configuration
 public class Config implements WebMvcConfigurer {
